c58f37909f3f42c36ca6e543ba750f7cedf2777c
Daniel <239256975+peek-chungus@users.noreply.github.com>
2026-03-26 03:47:38 -0700

    Update hyprctl.sh

--- hyprctl.sh
+++ hyprctl.sh
@@ -8,27 +8,27 @@ spotify="spotify-launcher"
 
 hyprctl dispatch workspace 1
 $terminal & disown
-sleep 1.5
+sleep 3
 
 hyprctl dispatch workspace 2
 $minecraft & disown
-sleep 1.5
+sleep 3
 
-hyprctl dispatch 5
+hyprctl dispatch workspace 5
 $terminal -e cmatrix -C magenta & disown
 $terminal -e peaclock --config-dir="${HOME}/.config/peaclock" & disown
 $terminal -e cava & disown
 $terminal -e htop & disown
-sleep 1.5
+sleep 2
 
 hyprctl dispatch workspace 6
-$brower & disown
-sleep 2
+$browser & disown
+sleep 5
 
 hyprctl dispatch workspace 7
 $spotify & disown
-sleep 1.5
+sleep 5
 hyprctl dispatch workspace 8
 $discord &>/dev/null & disown
-sleep 2.5
+sleep 5
 hyprctl dispatch workspace 1
